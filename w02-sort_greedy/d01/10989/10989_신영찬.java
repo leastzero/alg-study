6240cfa308ef4fed0982e21308f3420d470ac3cf
shin1488 <syc1488@naver.com>
2026-01-05 18:51:27 +0900

    10989 java solution

--- w02-sort_greedy/d01/10989/10989_신영찬.java
+++ w02-sort_greedy/d01/10989/10989_신영찬.java
@@ -7,19 +7,21 @@ class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         //TODO : 배열 만들어서 add
-        ArrayList<Integer> arr = new ArrayList<>();
+        int size = Integer.parseInt(br.readLine());
+        int[] brr = new int[size];
         String nextLine = br.readLine();
-        while(nextLine != null) {
-            arr.add(Integer.parseInt(nextLine));
+        while (nextLine != null) {
+            brr[Integer.parseInt(nextLine)] += 1;
             nextLine = br.readLine();
         }
 
-        //TODO : 배열 정렬
-        Collections.sort(arr);
-
         //TODO : 배열 출력
-        for(int i = 0; i < arr.size(); i++) {
-            bw.write(String.valueOf(arr.get(i)) + '\n');
+        for (int i = 0; i < size; i++) {
+            if (brr[i] != 0) {
+                for (int j = 0; j < brr[i]; j++) {
+                    bw.write(String.valueOf(i) + '\n');
+                }
+            }
         }
         bw.flush();
         br.close();
